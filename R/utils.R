0c1330c96c9ffa62d6ad0a07331b86d760f78de6
Doug Ashton <douglas.j.ashton@gmail.com>
2018-06-06 20:28:27 +0100

    isHoliday

--- R/utils.R
+++ R/utils.R
@@ -1,52 +1,17 @@
 
-# Rolling cumulative sum
+# Create a holiday feature for New York dates
 #
-# Directly equivalent to \code{zoo:rollsum(x,k,align='left')}
-#
-# @param x Numeric vector
-# @param k Integer size of rolling window
-#
-.cumsumroll <- function(x, k = 3L) {
-  if(k<2) {
-    y <- as.numeric(x)
-  } else {
-    y <- cumsum(x)[-(1:(k-1))] - cumsum(c(0,x[-(length(x):(length(x) - k +1))]))
-    if(!is.null(names(x))) {
-      names(y) <- names(x)[-(length(x):(length(x) - k + 2))] # like a zoo::rollsum(align="left")
-    }
-  }
-  y
-}
-
-# Create a logical sequence of dates
-#
-# Given a date range, create a date sequence with every day in between and then
-# create a vector which is true if it's a working day
-#
-# @param startDate
-# @param endDate
-# @param country
+# @param date A vector of dates on which to check holidays
 #
 # @return
-isWorking <- function(startDate, endDate, country="UK") {
+isHoliday <- function(date) {
 
-  # TODO Argument checking
 
-  holFunction <- switch(country, UK = timeDate::holidayLONDON,
-                        stop("Must be UK for now. Sorry."))
-
-  startDate <- as.Date(startDate)
-  endDate <- as.Date(endDate)
-
-  # Extract the years
-  sy = as.numeric(format(startDate, "%Y"))
-  ey = as.numeric(format(endDate, "%Y"))
-
-  # Create all the dates in between
-  dates <- seq(startDate, endDate, by = "day")
-
-  # TODO Allow other calendars
-  x <- timeDate::isBizday(timeDate::as.timeDate(dates), holidays = holFunction(sy:ey))
+  years <- lubridate::year(date)
+  
+  x <- timeDate::isHoliday(as.timeDate(date),
+                           holidayNYSE(unique(years)),
+                           wday=0:6)
 
   x
 
